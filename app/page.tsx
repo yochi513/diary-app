bc0e8f571c949e56d96edef0daa84c97a9720e87
yochi513 <yojrdkt@gmail.com>
2026-05-22 22:13:50 +0900

    日記アプリ作成途中

--- app/page.tsx
+++ app/page.tsx
@@ -1,65 +1,107 @@
-import Image from "next/image";
+"use client"
+import{useState}from "react";
+export default function Home() //画面全体
+{
+  //h1で大きいタイトル
+  //h2見出し
+  //classNameは見た目設定。
+  //div箱みたいな物
+  //=>処理する方法
+  //p 本文表示
+  //confirmブラウザ標準機能
 
-export default function Home() {
+  const [title,setTitle]= useState("");
+  const [content,setContent]= useState("");
+
+const [diaries,setDiaries]= useState<
+{title:string; content:string; date:string}[]
+>([]);
+
+const addDiary =()=>{
+if(title===""||content==="")return;
+const newDiary={
+title:title,
+content: content,
+date: new Date().toLocaleDateString(),
+};
+setDiaries([newDiary,...diaries]);
+
+setTitle("");
+setContent("");
+};
+const deleteDiary=(index:number)=>{
+
+  const check = confirm("本当に削除しますか？");
+
+  if(!check) return;
+
+  const newDiaries=diaries.filter(
+  (_,diaryIndex)=>diaryIndex!==index
+  );
+setDiaries(newDiaries);
+};
+
+
   return (
-    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
-        </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
-        </div>
-      </main>
-    </div>
+    <main className="min-h-screen bg-sky-100 p-8">
+      <h1 className="text-4xl font-bold mb-6 text-pink-500 ">
+         私の思い出
+      </h1>
+
+<div className="bg-white p-6 rounded-2xl shadow-md max-w-xl">
+
+<input
+type="text"
+placeholder="タイトルを入力"
+value={title}
+onChange={(e)=>setTitle(e.target.value)}
+className="w-full border p-1 rounded-lg mb-4 placeholder-gray-400 text-black"
+/>
+
+<textarea
+placeholder="今日の出来事を書こう・・・"
+value={content}
+onChange={(e)=>setContent(e.target.value)}
+className="w-full border p-3 rounded-lg mb-4 h-40 placeholder-gray-400 text-black"
+/>
+
+<button 
+onClick={addDiary}
+className="bg-blue-500 text-white px-10 py-4 rounded-2xl">
+  投稿する
+</button>
+
+      </div>
+
+<div className="max-w-xl">
+
+{diaries.map((diary,index)=>(
+<div
+key={index}
+className="bg-white p-5 rounded-2xl shadow-md mb-4"
+>
+<h2 className="text-2xl font-bold mb-6 text-black">
+  {diary.title}
+</h2>
+
+<p className="text-gray-500 mb-2">
+  {diary.date}
+</p>
+
+<p className="text-gray-800 whitespace-pre-wrap">
+  {diary.content}
+</p>
+
+<button
+onClick={()=>deleteDiary(index)}
+className="mt-4 bg-red-500 text-white px-4 py-2 rounded-lg">
+  削除
+</button>
+
+</div>
+))}
+
+</div>
+</main>
   );
 }
