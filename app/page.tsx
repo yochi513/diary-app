878914199e41bfa2107c4e91a7b628b89139d18d
yochi513 <yojrdkt@gmail.com>
2026-06-03 19:30:48 +0900

    編集UX変更・検索機能追加・その他UI改善

--- app/page.tsx
+++ app/page.tsx
@@ -10,13 +10,19 @@ export default function Home() //画面全体
   //=>処理する方法
   //p 本文表示
 
-
+  //日記の主要部分
   const [title,setTitle]= useState("");
   const [content,setContent]= useState("");
   const [image,setImage]=useState("");
   const [video,setVideo]=useState("");
-  const [editIndex,setEditIndex]=useState<number|null>(null);
 
+  //edit関連
+  const [editIndex,setEditIndex]=useState<number|null>(null);
+  const [editTitle,setEditTitle]=useState("");
+  const [editContent,setEditContent]=useState("");
+
+  //検索
+  const [search,setSearch]=useState("");
 
 const [diaries,setDiaries]= useState<
 {
@@ -91,6 +97,7 @@ localStorage.setItem(
 
 },[diaries]);
 
+//サイトアプリの表示させる部分↓
 return (
     <main className="min-h-screen bg-sky-100 p-8">
       <h1 className="text-4xl font-bold mb-6 text-pink-500 ">
@@ -116,30 +123,58 @@ onChange={(e)=>setContent(e.target.value)}
 className="w-full border p-3 rounded-lg mb-4 h-40 placeholder-gray-400 text-black"
 />
 
+<p className="text-black mb-1">
+画像を追加↓
+</p>
 
 <input
+//keyが変わる=別物認識：投稿した後の履歴が残らない
+key={image}
 //file=ファイル選択
 type="file"
 //image=画像選択
 accept="image/*"
 
+className="text-black mb-4 
+file:bg-blue-500
+file:text-white
+file:border-0
+file:px-1
+file;py-2
+file;rounded-lg"
+
 onChange={(e)=>{
 const file = e.target.files?.[0];
+
 if(!file) return;
+
 //ブラウザ表示するためのURL
 const imageUrl =URL.createObjectURL(file);
 setImage(imageUrl);
 }}
 />
 
+<p className="text-black mb-1">
+  動画を追加↓
+</p>
+
 <input
+key={video}
 type="file"
 accept="video/*"
 
-onChange={(e)=>{
+className="text-black mb-2 
+file:bg-blue-500
+file:text-white
+file:border-0
+file:px-1
+file;py-2
+file;rounded-lg"
 
+onChange={(e)=>{
 const file = e.target.files?.[0];
 if(!file) return;
+
 const videoUrl=URL.createObjectURL(file);
 setVideo(videoUrl);
 
@@ -154,16 +189,51 @@ className="bg-blue-500 text-white px-10 py-4 rounded-2xl">
 
       </div>
 
-<div className="max-w-xl">
+<div className="max-w-xl mt-8">
+//検索の部分
+  <input
+type="text"
+placeholder="検索"
+value={search}
+onChange={(e)=>setSearch(e.target.value)}
 
-{diaries.map((diary,index)=>(
+className="w-full border p-3 rounded-lg mb-4
+text-black placeholder-gray-400"
+/>
+
+{diaries.filter((diary)=>{
+return(
+
+  //.toLowerCase():全て小文字変換する
+  //.includes():if文みたいな物で()内の物と比べるため
+  //検索文字が含まれているかどうか判断してる
+  diary.title.toLowerCase().includes(search.toLowerCase())
+  
+  ||
+
+  diary.content.toLowerCase().includes(search.toLowerCase())
+
+);
+})
+.map((diary,index)=>(
 <div
 key={index}
 className="bg-white p-5 rounded-2xl shadow-md mb-4"
 >
+
+  {editIndex==index ?(
+<input
+value={editTitle}
+onChange={(e)=>setEditTitle(e.target.value)}
+
+className="w-full border p-2 rounded text-black mb-2"
+/>
+  ):(
+
 <h2 className="text-2xl font-bold mb-6 text-black">
   {diary.title}
 </h2>
+)}
 
 <p className="text-gray-500 mb-2">
   {diary.date}
@@ -191,9 +261,58 @@ className="w-full rounded-xl mb-4"
 />
 )}
 
+{editIndex==index ?(
+<textarea
+value={editContent}
+onChange={(e)=>setEditContent(e.target.value)}
+className="w-full border p-2 rounded text-black h-32"
+/>
+):(
 <p className="text-gray-800 whitespace-pre-wrap">
   {diary.content}
 </p>
+)}
+
+{editIndex===index &&(
+//<></>一つにまとめることができる
+<>
+
+<button
+onClick={()=>{
+const newDiaries=[...diaries];
+
+newDiaries[index]={
+  ...newDiaries[index],
+  title:editTitle,
+  content:editContent,
+};
+setDiaries(newDiaries);
+setEditIndex(null);
+setEditTitle("");
+setEditContent("");
+}}
+
+className="bg-green-500 tetx-white px-4 py-2 rounded-lg mr-2"
+
+>
+  保存
+</button>
+
+<button
+onClick={()=>{
+setEditIndex(null);
+setEditTitle("");
+setEditContent("");  
+}}
+className="bg-gray-500 text-white px-4 py-2 rounded-lg"
+>
+  キャンセル
+</button>
+
+</>
+
+)}
+
 
 <button
 onClick={()=>deleteDiary(index)}
@@ -203,12 +322,13 @@ className="mt-4 bg-red-500 text-white px-4 py-2 rounded-lg">
 
 <button
 onClick={()=>{
-console.log("編集押された")
-   setTitle(diary.title);
-   setContent(diary.content);
-   setImage(diary.image);
-   setVideo(diary.video);
+
    setEditIndex(index);
+   //setTitle(diary.title);
+   //setContent(diary.content);
+  // setImage(diary.image);
+  // setVideo(diary.video);
+
 }}
 className="mt-4 mr-2 bg-yellow-500 text-white px-4 py-2 rounded-lg"
 >
